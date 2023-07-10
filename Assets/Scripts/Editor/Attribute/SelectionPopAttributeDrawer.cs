a31e98402dbcce56d1576b35c5fc02ec6171c569
AKIRA <602646397@qq.com>
2023-07-10 14:38:36 +0800

    添加SelectionPop特性

---
+++ Assets/Scripts/Editor/Attribute/SelectionPopAttributeDrawer.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace AKIRA.Attribute.Editor {
+    [CustomPropertyDrawer(typeof(SelectionPopAttribute))]
+    public class SelectionPopAttributeDrawer : PropertyDrawer {
+        private bool extendY = false;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            var selection = attribute as SelectionPopAttribute;
+            var type = selection.type;
+
+            var strValue = property.stringValue;
+            var values = type.GetFields();
+            var index = 0;
+            List<GUIContent> names = new List<GUIContent>();
+            for (int i = 0; i < values.Length; i++) {
+                var name = values[i].GetRawConstantValue().ToString();
+                if (String.IsNullOrEmpty(name))
+                    continue;
+                if (strValue.Equals(name))
+                    index = i;
+                names.Add(new GUIContent(name));
+            }
+
+            extendY = names.Count == 0;
+            if (extendY) {
+                Rect strPosition = new Rect(position.x, position.y, position.width, position.height / 2);
+                property.stringValue = EditorGUI.TextField(strPosition, label, property.stringValue);
+                Rect boxPosition = new Rect(position.x, position.y + strPosition.height + 3f, position.width, position.height / 2 - 3f);
+                EditorGUI.HelpBox(boxPosition, $"No const value in {type}", MessageType.Info);
+            } else {
+                index = EditorGUI.Popup(position, label, index, names.ToArray());
+                property.stringValue = names[index].text;
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+            return base.GetPropertyHeight(property, label) * (extendY ? 2.2f : 1);
+        }
+}
+}
--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # 修改记录
 
-**Version: 0.0.1**
+**Version: 0.0.0**
 
 * 建立项目
-
   * URP项目
-  * [ECS Setup]()
+  * [ECS Setup](https://docs.unity3d.com/Packages/com.unity.entities@1.0/manual/getting-started-installation.html)
   * [DOTS Setup](https://docs.unity3d.com/Packages/com.unity.entities@0.17/manual/install_setup.html)
-* 文件夹
 
+* 文件夹
   * Res：资源
   * Plugins：插件
   * Scripts：脚本
@@ -17,3 +16,13 @@
       * 其他单例：SystemBase
     * Editor：编辑器下代码
     * Test：测试
+
+* 添加通用代码
+  * Attribute
+    * SelectionPop：常量数据的序列化
+![](./Assets/Res/ReadmeLinks/Version_0.0.0/1.png)
+![](./Assets/Res/ReadmeLinks/Version_0.0.0/2.png)
+  * Common
+    * GameData：数据
+    * ISystem：System接口
+    * Singleton：CSharp单例
