ebfbb1450960fc30c469f8c8c3073976fdd901ca
AKIRA <602646397@qq.com>
2023-08-02 15:28:18 +0800

    CameraFollowSystem

--- Assets/Scripts/Runtime/Module/Camera/CameraSystem.cs
+++ Assets/Scripts/Runtime/Module/Camera/CameraSystem.cs
@@ -2,126 +2,53 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using AKIRA.Behaviour.Camera;
 using Unity.Entities;
 using Unity.Burst;
+using Unity.Collections;
+using Unity.Transforms;
 
 namespace AKIRA.Behaviour.Camera {
-    public partial struct CameraSystem : ISystem {
+    // public partial struct CameraSystem : ISystem {
+    //     [BurstCompile]
+    //     public void OnCreate(ref SystemState state) { }
+
+    //     [BurstCompile]
+    //     public void OnUpdate(ref SystemState state) { }
+    // }
+
+    [UpdateInGroup(typeof(LateSimulationSystemGroup))]
+    public partial struct CameraFollowSystem : ISystem {
+        // 跟随实体
+        private Entity target;
+
         [BurstCompile]
         public void OnCreate(ref SystemState state) {
-            state.RequireForUpdate<CameraTagAuthoring>();
+            state.RequireForUpdate<CameraFollowComponent>();
         }
 
-        [BurstCompile]
         public void OnUpdate(ref SystemState state) {
-            
-        }
+            // 获得CameraFollowComponent组件实体
+            if (target == Entity.Null) {
+                var query = SystemAPI.QueryBuilder().WithAll<CameraFollowComponent>().Build();
+                var targets = query.ToEntityArray(Allocator.Temp);
+                if (targets.Length == 0)
+                    return;
+                target = targets[0];
+            }
 
-        public GameObject GetCamera() {
-            return default;
-        }
-    }
-}
+            var camera = CameraExtend.Transform;
+            var targetTrans = SystemAPI.GetComponent<LocalToWorld>(target);
+            var followComponent = SystemAPI.GetComponent<CameraFollowComponent>(target);
 
-/// <summary>
-/// 摄像机扩展
-/// </summary>
-public static class CameraExtend {
-    private static Camera mainCamera;
-    /// <summary>
-    /// 主摄像机
-    /// </summary>
-    /// <value></value>
-    public static Camera MainCamera {
-        get {
-            if (mainCamera == null)
-                mainCamera = Camera.main;
-            return mainCamera;
-        }
-    }
+            // 判断是否跟随
+            if (followComponent.follow)
+                camera.position = 
+                    Vector3.Lerp(camera.position, targetTrans.Position + followComponent.offset, SystemAPI.Time.DeltaTime * followComponent.lerpSpeed);
 
-    /// <summary>
-    /// 表现字典
-    /// </summary>
-    /// <typeparam name="Type"></typeparam>
-    /// <typeparam name="CameraBehaviour"></typeparam>
-    /// <returns></returns>
-    private static Dictionary<Type, CameraBehaviour> CameraMap = new Dictionary<Type, CameraBehaviour>();
-
-    /// <summary>
-    /// Transform
-    /// </summary>
-    public static Transform Transform => MainCamera.transform;
-
-    /// <summary>
-    /// 是否存在主摄像机
-    /// </summary>
-    public static bool ExistMainCamera => MainCamera != null;
-
-    /// <summary>
-    /// 摄像机标签
-    /// </summary>
-    /// <typeparam name="string"></typeparam>
-    /// <typeparam name="GameObject"></typeparam>
-    /// <returns></returns>
-    private static Dictionary<string, GameObject> TagMap = new Dictionary<string, GameObject>();
-
-    /// <summary>
-    /// 注册摄像机表现脚本
-    /// </summary>
-    /// <param name="behaviour"></param>
-    /// <typeparam name="T"></typeparam>
-    internal static void RegistCameraBehaviour<T>(T behaviour) where T : CameraBehaviour {
-        var type = behaviour.GetType();
-        if (CameraMap.ContainsKey(type))
-            return;
-        CameraMap.Add(type, behaviour);
-    }
-
-    /// <summary>
-    /// 移除摄像机表现脚本
-    /// </summary>
-    /// <param name="behaviour"></param>
-    /// <typeparam name="T"></typeparam>
-    internal static void RemoveCameraBehaviour<T>(T behaviour) where T : CameraBehaviour {
-        var type = behaviour.GetType();
-        if (!CameraMap.ContainsKey(type))
-            return;
-        CameraMap.Remove(type);
-    }
-
-    /// <summary>
-    /// 获得摄像机脚本
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public static T GetCameraBehaviour<T>() where T : CameraBehaviour {
-        var type = typeof(T);
-        return CameraMap.ContainsKey(type) ? CameraMap[type] as T : default;
-    }
-    
-    /// <summary>
-    /// 添加摄像机
-    /// </summary>
-    /// <param name="tag"></param>
-    internal static void AddCamera(string tag, GameObject camera) {
-        if (TagMap.ContainsKey(tag)) {
-            $"{camera} add error: Camera contains tag => {tag}".Error();
-        } else {
-            $"{camera} add, tag => {tag}".Log();
-            TagMap.Add(tag, camera);
+        
+            // 判断是否看着
+            if (followComponent.lookAt)
+                camera.LookAt(targetTrans.Position);
         }
     }
-
-    /// <summary>
-    /// 获得摄像机，主要用于Cine虚拟摄像机，主摄像机还是通过MainCamera获得
-    /// </summary>
-    /// <param name="tag"></param>
-    public static GameObject GetCamera(string tag) {
-        if (!TagMap.ContainsKey(tag))
-            return default;
-        else
-            return TagMap[tag];
-    }
-
 }
--- Assets/Scripts/Runtime/Module/Camera/CameraTagAuthoring.cs
+++ Assets/Scripts/Runtime/Module/Camera/CameraTagAuthoring.cs
@@ -1,7 +1,4 @@
 using AKIRA.Attribute;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Entities.Serialization;
 using UnityEngine;
 
 namespace AKIRA.Behaviour.Camera {
@@ -16,27 +13,9 @@ namespace AKIRA.Behaviour.Camera {
         [SelectionPop(typeof(GameData.Camera))]
         private string cameraTag;
 
-        // private void Awake() {
-        //     CameraExtend.AddCamera(cameraTag, this.gameObject);
-        //     GameObject.Destroy(this);
-        // }
-
-        class Baker : Unity.Entities.Baker<CameraTagAuthoring>
-        {
-            public override void Bake(CameraTagAuthoring authoring)
-            {
-                var manager = World.DefaultGameObjectInjectionWorld.EntityManager;
-                manager.Log();
-                var entity = GetEntity(TransformUsageFlags.Dynamic);
-                entity.Log();
-                AddComponent(entity, new CameraTagComponent {
-                    tag = authoring.cameraTag
-                });
-            }
+        private void Awake() {
+            CameraExtend.AddCamera(cameraTag, this.gameObject);
+            GameObject.Destroy(this);
         }
-    }
-
-    public struct CameraTagComponent : IComponentData {
-        public FixedString32Bytes tag;
     }
 }
