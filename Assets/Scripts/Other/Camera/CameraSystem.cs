b716523ca55b5d6f5e9ca300ff4f87168e714c1f
AKIRA <602646397@qq.com>
2023-11-20 14:22:42 +0800

    更新CameraSystem

--- Assets/Scripts/Other/Camera/CameraSystem.cs
+++ Assets/Scripts/Other/Camera/CameraSystem.cs
@@ -1,8 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Transforms;
+using AKIRA.Manager;
+using System.Linq;
+using System.Threading.Tasks;
+
+
 #if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
 #if UNITY_ANDROID || UNITY_IOS
@@ -12,6 +18,8 @@ using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 #endif
 
 namespace AKIRA.Behaviour.Camera {
+#if UNITY_2022_1_OR_NEWER
+    #region UNITY_2022 Entity System
     // public partial struct CameraSystem : ISystem {
     //     [BurstCompile]
     //     public void OnCreate(ref SystemState state) { }
@@ -80,11 +88,11 @@ namespace AKIRA.Behaviour.Camera {
 
         protected override void OnUpdate() {
 #if ENABLE_INPUT_SYSTEM
-#if UNITY_ANDROID || UNITY_IOS
+    #if UNITY_ANDROID || UNITY_IOS
             if (Touch.activeTouches.Count == 0 && dragObject != null)
-#else
+    #else
             if (Mouse.current.leftButton.wasReleasedThisFrame && dragObject != null)
-#endif
+    #endif
 #else
             if (Input.GetMouseUp(0))
 #endif
@@ -94,21 +102,21 @@ namespace AKIRA.Behaviour.Camera {
             }
 
 #if ENABLE_INPUT_SYSTEM
-#if UNITY_ANDROID || UNITY_IOS
+    #if UNITY_ANDROID || UNITY_IOS
             if (Touch.activeTouches.Count > 0 && dragObject == null)
-#else
+    #else
             if (Mouse.current.leftButton.wasPressedThisFrame && dragObject == null)
-#endif
+    #endif
 #else
             if (Input.GetMouseDown(0))
 #endif
             {
 #if ENABLE_INPUT_SYSTEM
-#if UNITY_ANDROID || UNITY_IOS
+    #if UNITY_ANDROID || UNITY_IOS
                 Ray ray = CameraExtend.MainCamera.ScreenPointToRay(Touch.activeTouches[0].screenPosition);
-#else
+    #else
                 Ray ray = CameraExtend.MainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-#endif
+    #endif
 #else
                 Ray ray = CameraExtend.MainCamera.ScreenPointToRay(Input.mousePosition);
 #endif
@@ -157,4 +165,167 @@ namespace AKIRA.Behaviour.Camera {
             }
         }
     }
+    #endregion
+#else
+    #region UNITY_2021(or lower) AKIRA.System
+    public class CameraSystem : Singleton<CameraSystem>, IUpdate {
+        // 跟随目标数组
+        private List<CameraFollowAuthoring> targets = new();
+        /// <summary>
+        /// 当前跟随目标
+        /// </summary>
+        public CameraFollowAuthoring CurFollowTarget => targets?.LastOrDefault();
+
+        // 平滑时间
+        private const float SmoothTime = 1f;
+        // 
+        private Vector3 velocity;
+
+        // 是否被占用（繁忙）
+        private bool isBusying = false;
+
+        // 拖拽目标
+        private IDrag dragObject;
+
+        protected CameraSystem() {}
+
+        public override async Task Initialize() {
+            await Task.Yield();
+            this.Regist();
+        }
+
+        /// <summary>
+        /// 添加跟随目标
+        /// </summary>
+        /// <param name="target"></param>
+        public void AddLookTarget(CameraFollowAuthoring target) {
+            if (targets.Contains(target))
+                return;
+            targets.Add(target);
+        }
+
+        /// <summary>
+        /// 移除跟随目标
+        /// </summary>
+        /// <param name="target"></param>
+        public void RemoveLookTarget(CameraFollowAuthoring target) {
+            if (!targets.Contains(target))
+                return;
+            targets.Remove(target);
+        }
+
+        public void GameUpdate() {
+            // 忙碌中
+            if (isBusying)
+                return;
+            
+            var target = CurFollowTarget;
+            // 当前跟随目标为空
+            if (target == default) {
+                return;
+            } else {
+                if (target.isIntervalLook) {
+                    SetIntervalWorldLocation(target);
+                } else {
+                    SetWorldLocation(target);
+                }
+            }
+
+#if ENABLE_INPUT_SYSTEM
+    #if UNITY_ANDROID || UNITY_IOS
+            if (Touch.activeTouches.Count == 0 && dragObject != null)
+    #else
+            if (Mouse.current.leftButton.wasReleasedThisFrame && dragObject != null)
+    #endif
+#else
+            if (Input.GetMouseUp(0))
+#endif
+            {
+                dragObject.OnDragUp();
+                dragObject = null;
+            }
+
+#if ENABLE_INPUT_SYSTEM
+    #if UNITY_ANDROID || UNITY_IOS
+            if (Touch.activeTouches.Count > 0 && dragObject == null)
+    #else
+            if (Mouse.current.leftButton.wasPressedThisFrame && dragObject == null)
+    #endif
+#else
+            if (Input.GetMouseDown(0))
+#endif
+            {
+#if ENABLE_INPUT_SYSTEM
+    #if UNITY_ANDROID || UNITY_IOS
+                Ray ray = CameraExtend.MainCamera.ScreenPointToRay(Touch.activeTouches[0].screenPosition);
+    #else
+                Ray ray = CameraExtend.MainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+    #endif
+#else
+                Ray ray = CameraExtend.MainCamera.ScreenPointToRay(Input.mousePosition);
+#endif
+                var hits = Physics.RaycastAll(ray, System.Single.MaxValue);
+                foreach (var hit in hits) {
+                    // 拿到第一个IDrag
+                    if (hit.transform.TryGetComponent<IDrag>(out dragObject)) {
+                        dragObject.OnDragDown();
+                        break;
+                    }
+                }
+            }
+
+            if (dragObject != null)
+                dragObject.OnDrag();
+
+#if ENABLE_INPUT_SYSTEM
+    #if UNITY_ANDROID || UNITY_IOS
+            if (Touch.activeTouches.Count > 0) {
+                Ray ray = CameraExtend.MainCamera.ScreenPointToRay(Touch.fingers[0].screenPosition);
+    #else
+            if (Mouse.current.leftButton.wasPressedThisFrame) {
+                Ray ray = CameraExtend.MainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+    #endif
+#else
+            if (Input.GetMouseButtonDown(0)) {
+                Ray ray = CameraExtend.MainCamera.ScreenPointToRay(Input.mousePosition);
+#endif
+                if (Physics.Raycast(ray, out RaycastHit hit, System.Single.MaxValue)) {
+                    if (hit.transform.TryGetComponent<IClick>(out IClick click)) {
+                        click.OnClick();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 设置摄像机位置和转向
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="rotation"></param>
+        private void SetWorldLocation(CameraFollowAuthoring target) {
+            // 跟随摄像机，一般摄像机
+            var camera = CameraExtend.GetCamera(GameData.Camera.Main).transform;
+            camera.position = Vector3.SmoothDamp(camera.position, target.GetPosition(), ref velocity, SmoothTime);
+            camera.rotation = Quaternion.Slerp(camera.rotation, target.GetRotation(), SmoothTime * Time.deltaTime);
+        }
+
+        /// <summary>
+        /// 设置摄像机短暂停留位置和转向
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        private async void SetIntervalWorldLocation(CameraFollowAuthoring target) {
+            float time = target.lookTime + SmoothTime;
+            isBusying = true;
+            while (time >= 0) {
+                time -= Time.deltaTime;
+                SetWorldLocation(target);
+                await Task.Yield();
+            }
+            target.enabled = false;
+            isBusying = false;
+        }
+    }
+    #endregion
+#endif
 }
--- Assets/Scripts/Other/Camera/Component/CameraFollowAuthoring.cs
+++ Assets/Scripts/Other/Camera/Component/CameraFollowAuthoring.cs
@@ -3,6 +3,8 @@ using Unity.Mathematics;
 using UnityEngine;
 
 namespace AKIRA.Behaviour.Camera {
+#if UNITY_2022_1_OR_NEWER
+    #region UNITY_2022 Entity System
     /// <summary>
     /// 摄像机跟随
     /// </summary>
@@ -54,4 +56,58 @@ namespace AKIRA.Behaviour.Camera {
         // 是否一直盯着
         public bool lookAt;
     }
+    #endregion
+#else
+    #region UNITY_2021(or lower)
+    public class CameraFollowAuthoring : MonoBehaviour {
+        // 目标
+        [SerializeField]
+        private Transform lookTarget;
+        // 插值
+        [SerializeField]
+        private Vector3 offsetPosition;
+        [SerializeField]
+        private Vector3 offsetRotation;
+        
+        [Space]
+        // 是否短时间看
+        public bool isIntervalLook = false;
+        // 时间
+        public float lookTime;
+
+        private void OnEnable() {
+            CameraSystem.Instance.AddLookTarget(this);
+        }
+
+        private void OnDisable() {
+            CameraSystem.Instance.RemoveLookTarget(this);
+        }
+
+        /// <summary>
+        /// 获得坐标
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 GetPosition() => lookTarget.position + offsetPosition;
+        /// <summary>
+        /// 获得朝向
+        /// </summary>
+        /// <returns></returns>
+        public Quaternion GetRotation() => Quaternion.Euler(lookTarget.eulerAngles + offsetRotation);
+
+        #if UNITY_EDITOR
+        /// <summary>
+        /// 设置插值
+        /// </summary>
+        [ContextMenu("Set Offset")]
+        private void SetOffset() {
+            if (lookTarget == null)
+                return;
+
+            offsetPosition = this.transform.position - lookTarget.position;
+            offsetRotation = this.transform.eulerAngles - lookTarget.eulerAngles;
+        }
+        #endif
+    }
+    #endregion
+#endif
 }
