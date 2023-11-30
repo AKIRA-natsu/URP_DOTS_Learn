dc3aaabf9994dd1c3098d096240681acb7527b00
AKIRA-natsu <57622517+AKIRA-natsu@users.noreply.github.com>
2023-11-30 14:52:33 +0800

    Update AssetSystem.cs
    修复平台报错

--- Assets/Scripts/Runtime/Module/AssetBundle/AssetSystem.cs
+++ Assets/Scripts/Runtime/Module/AssetBundle/AssetSystem.cs
@@ -27,9 +27,9 @@ namespace AKIRA.Manager {
         protected AssetSystem() {
             config = GameData.Path.AssetBundleConfig.Load<AssetBundleConfig>();
             preformPath = 
-#if UNITY_ANDROID
+#if UNITY_ANDROID && !UNITY_EDITOR
                 "jar:file://" + Application.dataPath + "!/assets";
-#elif UNITY_IOS
+#elif UNITY_IOS && !UNITY_EDITOR
                 Application.dataPath + "/Raw";
 #else
                 Application.streamingAssetsPath;
@@ -55,9 +55,20 @@ namespace AKIRA.Manager {
                 yield break;
             }
 
-            if (config.useWebRequestTest) {
+#if UNITY_EDITOR
+            if (!config.useWebRequestTest) {
                 for (int i = 0; i < paths.Length; i++) {
                     var path = paths[i];
+                    $"AB: AssetBundle.LoadFromFile 尝试读取 => {Path.Combine(preformPath, path)}".Log(GameData.Log.Editor);
+                    bundles[i] = AssetBundle.LoadFromFile(Path.Combine(preformPath, path));
+                    LoadProgress = (float)i / path.Length;
+                }
+            } else
+#endif
+            {
+                for (int i = 0; i < paths.Length; i++) {
+                    var path = paths[i];
+                    $"AB: UnityWebRequestAssetBundle 尝试读取 => {Path.Combine(preformPath, path)}".Log(GameData.Log.Editor);
                     var request = UnityWebRequestAssetBundle.GetAssetBundle(Path.Combine(preformPath, path));
                     yield return request.SendWebRequest();
 
@@ -66,12 +77,6 @@ namespace AKIRA.Manager {
                     }
                     
                     bundles[i] = DownloadHandlerAssetBundle.GetContent(request);
-                    LoadProgress = (float)i / path.Length;
-                }
-            } else {
-                for (int i = 0; i < paths.Length; i++) {
-                    var path = paths[i];
-                    bundles[i] = AssetBundle.LoadFromFile(Path.Combine(preformPath, path));
                     LoadProgress = (float)i / path.Length;
                 }
             }
