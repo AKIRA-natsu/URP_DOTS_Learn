774b7fc00fce49362de037df62fb192bad08db67
AKIRA <602646397@qq.com>
2023-12-08 17:50:29 +0800

    SelectionPop限制的HelpBox

--- Assets/Scripts/Editor/Attribute/SelectionPopAttributeDrawer.cs
+++ Assets/Scripts/Editor/Attribute/SelectionPopAttributeDrawer.cs
@@ -9,31 +9,35 @@ namespace AKIRA.Attribute.Editor {
         private bool extendY = false;
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-            var selection = attribute as SelectionPopAttribute;
-            var type = selection.type;
+            if (property.propertyType == SerializedPropertyType.String) {
+                var selection = attribute as SelectionPopAttribute;
+                var type = selection.type;
 
-            var strValue = property.stringValue;
-            var values = type.GetFields();
-            var index = 0;
-            List<GUIContent> names = new List<GUIContent>();
-            for (int i = 0; i < values.Length; i++) {
-                var name = values[i].GetRawConstantValue().ToString();
-                if (String.IsNullOrEmpty(name))
-                    continue;
-                if (strValue.Equals(name))
-                    index = i;
-                names.Add(new GUIContent(name));
-            }
+                var strValue = property.stringValue;
+                var values = type.GetFields();
+                var index = 0;
+                List<GUIContent> names = new List<GUIContent>();
+                for (int i = 0; i < values.Length; i++) {
+                    var name = values[i].GetRawConstantValue().ToString();
+                    if (String.IsNullOrEmpty(name))
+                        continue;
+                    if (strValue.Equals(name))
+                        index = i;
+                    names.Add(new GUIContent(name));
+                }
 
-            extendY = names.Count == 0;
-            if (extendY) {
-                Rect strPosition = new Rect(position.x, position.y, position.width, position.height / 2);
-                property.stringValue = EditorGUI.TextField(strPosition, label, property.stringValue);
-                Rect boxPosition = new Rect(position.x, position.y + strPosition.height + 3f, position.width, position.height / 2 - 3f);
-                EditorGUI.HelpBox(boxPosition, $"No const value in {type}", MessageType.Info);
+                extendY = names.Count == 0;
+                if (extendY) {
+                    Rect strPosition = new Rect(position.x, position.y, position.width, position.height / 2);
+                    property.stringValue = EditorGUI.TextField(strPosition, label, property.stringValue);
+                    Rect boxPosition = new Rect(position.x, position.y + strPosition.height + 3f, position.width, position.height / 2 - 3f);
+                    EditorGUI.HelpBox(boxPosition, $"No const value in {type}", MessageType.Info);
+                } else {
+                    index = EditorGUI.Popup(position, label, index, names.ToArray());
+                    property.stringValue = names[index].text;
+                }
             } else {
-                index = EditorGUI.Popup(position, label, index, names.ToArray());
-                property.stringValue = names[index].text;
+                EditorGUI.HelpBox(position, $"{typeof(SelectionPopAttribute)} is only support for {typeof(string)}", MessageType.Warning);
             }
         }
 
